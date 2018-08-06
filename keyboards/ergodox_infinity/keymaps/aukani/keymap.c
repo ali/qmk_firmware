df5961d96e3328e6c20ae341ddd7fb1f64eb35e9
Ali Ukani <ali.ukani@gmail.com>
2018-08-06 00:51:23 -0400

    customize layout
    https://configure.ergodox-ez.com/keyboard_layouts/mgexgm

--- keyboards/ergodox_infinity/keymaps/aukani/keymap.c
+++ keyboards/ergodox_infinity/keymaps/aukani/keymap.c
@@ -6,6 +6,7 @@
 #define L0 0 // default layer
 #define L1 1
 #define L2 2
+#define L3 3
 
 enum custom_keycodes {
   PLACEHOLDER = SAFE_RANGE, // can always be here
@@ -15,180 +16,14 @@ enum custom_keycodes {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[L0] = LAYOUT_ergodox(  // layer 0 : default
-/*
- * left hand
- *    +-------+-----+-----+-----+-----+-----+-----+
- *    |   =   |  0  |  2  |  3  |  4  |  5  | ESC |
- *    +-------+-----+-----+-----+-----+-----+-----+
- *    |   \   |  Q  |  W  |  E  |  R  |  T  |LCK-1|
- *    +-------+-----+-----+-----+-----+-----+     |
- *    |  TAB  |  A  |  S  |  D  |  F  |  G  +-----+
- *    +-------+-----+-----+-----+-----+-----+ f1  |
- *    | LSHIFT|  Z  |  X  |  C  |  V  |  B  |     |
- *    +-+-----+-----+-----+-----+-----+-----+-----+
- *      |LGUI |  `  |  \  |LEFT |RIGHT|
- *      +-----+-----+-----+-----+-----+   +-----+-----+
- *                                        |LCTRL| LALT|
- *                                  +-----+-----+-----+
- *                                  |     |     | HOME|
- *                                  |BKSPC| DEL +-----+
- *                                  |     |     | END |
- *                                  +-----+-----+-----+
- */
-        KC_EQL,  KC_1,   KC_2,    KC_3,    KC_4,    KC_5, KC_ESC,
-        KC_BSLS, KC_Q,   KC_W,    KC_E,    KC_R,    KC_T, TG(L1),
-        KC_TAB,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,
-        KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, MO(L1),
-        KC_LGUI, KC_GRV, KC_BSLS, KC_LEFT, KC_RGHT,
-                                                      KC_LCTL, KC_LALT,
-                                                               KC_HOME,
-                                             KC_BSPC, KC_DELT, KC_END,
-/* right hand
- *        +-----+-----+-----+-----+-----+-----+-------+
- *        |LCK-2|  6  |  7  |  8  |  9  |  0  |   -   |
- *        +-----+-----+-----+-----+-----+-----+-------+
- *        |  [  |  Y  |  U  |  I  |  O  |  P  |   ]   |
- *        |     +-----+-----+-----+-----+-----+-------+
- *        +-----+  H  |  J  |  K  |  L  |  ;  |   '   |
- *        | f1  +-----+-----+-----+-----+-----+-------+
- *        |     |  N  |  M  |  ,  |  .  |  /  | RSHIFT|
- *        +-----+-----+-----+-----+-----+-----+-----+-+
- *                    |LEFT |DOWN | UP  |RIGHT| RGUI|
- *    +-----+-----+   +-----+-----+-----+-----+-----+
- *    | RALT|RCTRL|
- *    +-----+-----+-----+
- *    | PGUP|     |     |
- *    +-----+ ENT | SPC |
- *    | PGDN|     |     |
- *    +-----+-----+-----+
- */
-             TG(L2),  KC_6,   KC_7,    KC_8,    KC_9,   KC_0,     KC_MINS,
-             KC_LBRC, KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,     KC_RBRC,
-                      KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN,  KC_QUOT,
-             MO(L1),  KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT,
-                              KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, KC_RGUI,
-             KC_RALT, KC_RCTL,
-             KC_PGUP,
-             KC_PGDN, KC_ENT, KC_SPC
-    ),
-
-[L1] = LAYOUT_ergodox( // layer 1 : function layers
-/* left hand
- *    +-------+-----+-----+-----+-----+-----+-----+
- *    |       | f1  | f2  | f3  | f4  | f5  | f11 |
- *    +-------+-----+-----+-----+-----+-----+-----+
- *    |       |     |     |     |     |     |     |
- *    +-------+-----+-----+-----+-----+-----+     |
- *    |       |     |     |     |     |     +-----+
- *    +-------+-----+-----+-----+-----+-----+     |
- *    |       |     |     |     |     |     |     |
- *    +-+-----+-----+-----+-----+-----+-----+-----+
- *      |     |     |     |     |     |
- *      +-----+-----+-----+-----+-----+   +-----+-----+
- *                                        |     |     |
- *                                  +-----+-----+-----+
- *                                  |     |     |     |
- *                                  |     |     +-----+
- *                                  |     |     |     |
- *                                  +-----+-----+-----+
- */
-       KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                                                      KC_TRNS, KC_TRNS,
-                                                               KC_TRNS,
-                                             KC_TRNS, KC_TRNS, KC_TRNS,
-/* right hand
- *        +-----+-----+-----+-----+-----+-----+-------+
- *        | f12 | f6  | f7  | f8  | f9  | f10 |       |
- *        +-----+-----+-----+-----+-----+-----+-------+
- *        |     |     |     |     |     |     |       |
- *        |     +-----+-----+-----+-----+-----+-------+
- *        +-----+     |     |     |     |     |       |
- *        |     +-----+-----+-----+-----+-----+-------+
- *        |     |     |     |     |     |     |       |
- *        +-----+-----+-----+-----+-----+-----+-----+-+
- *                    |     |     |     |     |     |
- *    +-----+-----+   +-----+-----+-----+-----+-----+
- *    |     |     |
- *    +-----+-----+-----+
- *    |     |     |     |
- *    +-----+     |     |
- *    |     |     |     |
- *    +-----+-----+-----+
- */
-       KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
-       KC_TRNS, KC_TRNS,
-       KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS
-),
-[L2] = LAYOUT_ergodox(
-/* Left hand
- *    +-------+-----+-----+-----+-----+-----+-----+
- *    | FLASH |     |     |     |     |     |     |
- *    +-------+-----+-----+-----+-----+-----+-----+
- *    |       |     |     |     |     |     |     |
- *    +-------+-----+-----+-----+-----+-----+     |
- *    |       |     |     |     |     |     +-----+
- *    +-------+-----+-----+-----+-----+-----+     |
- *    |       |     |     |     |     |     |     |
- *    +-+-----+-----+-----+-----+-----+-----+-----+
- *      |     |     |     |     |     |
- *      +-----+-----+-----+-----+-----+   +-----+-----+
- *                                        |     |     |
- *                                  +-----+-----+-----+
- *                                  |     |     |     |
- *                                  |     |     +-----+
- *                                  |     |     |     |
- *                                  +-----+-----+-----+
- */
-       RESET,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                                           KC_TRNS, KC_TRNS,
-                                                    KC_TRNS,
-                                  KC_TRNS, KC_TRNS, KC_TRNS,
-/* right hand
- *        +-----+-----+-----+-----+-----+-----+-------+
- *        |     |     |NMLCK| P/  | P*  | P-  |       |
- *        +-----+-----+-----+-----+-----+-----+-------+
- *        |     |     | P7  | P8  | P9  | P+  |       |
- *        |     +-----+-----+-----+-----+-----+-------+
- *        +-----+     | P4  | P5  | P6  | P+  |       |
- *        |     +-----+-----+-----+-----+-----+-------+
- *        |     |     | P1  | P2  | P3  |PENT |       |
- *        +-----+-----+-----+-----+-----+-----+-----+-+
- *                    |     |     | P.  |PENT |     |
- *    +-----+-----+   +-----+-----+-----+-----+-----+
- *    |     |     |
- *    +-----+-----+-----+
- *    |     |     |     |
- *    +-----+     | P0  |
- *    |     |     |     |
- *    +-----+-----+-----+
- */
-       KC_TRNS,  KC_TRNS, KC_NUMLOCK, KC_KP_SLASH, KC_KP_ASTERISK, KC_KP_MINUS, KC_TRNS,
-       KC_TRNS,  KC_TRNS, KC_KP_7,    KC_KP_8,     KC_KP_9,        KC_KP_PLUS,  KC_TRNS,
-                 KC_TRNS, KC_KP_4,    KC_KP_5,     KC_KP_6,        KC_KP_PLUS,  KC_TRNS,
-       KC_TRNS,  KC_TRNS, KC_KP_1,    KC_KP_2,     KC_KP_3,        KC_KP_ENTER, KC_TRNS,
-                          KC_TRNS,    KC_TRNS,     KC_KP_DOT,      KC_KP_ENTER, KC_TRNS,
-       KC_TRNS, KC_TRNS,
-       KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_KP_0
-),
+  [L0] = LAYOUT_ergodox(KC_EQUAL,KC_1,KC_2,KC_3,KC_4,KC_5,KC_ESCAPE,KC_TAB,KC_Q,KC_W,KC_E,KC_R,KC_T,KC_LBRACKET,KC_LCTL,KC_A,KC_S,KC_D,KC_F,KC_G,KC_LSPO,KC_Z,KC_X,KC_C,KC_V,KC_B,MO(1),KC_CAPSLOCK,KC_GRAVE,KC_LCTL,KC_LALT,KC_LGUI,KC_LGUI,KC_LALT,KC_HOME,KC_BSPACE,KC_DELETE,KC_END,TG(2),KC_6,KC_7,KC_8,KC_9,KC_0,KC_MINUS,KC_RBRACKET,KC_Y,KC_U,KC_I,KC_O,KC_P,KC_BSLASH,KC_H,KC_J,KC_K,KC_L,KC_SCOLON,KC_QUOTE,MO(2),KC_N,KC_M,KC_COMMA,KC_DOT,KC_SLASH,KC_RSPC,KC_RGUI,KC_RALT,MEH_T(KC_NO),ALL_T(KC_NO),TT(1),KC_RCTRL,KC_RGUI,KC_PGUP,KC_PGDOWN,KC_ENTER,KC_SPACE),
+  [L1] = LAYOUT_ergodox(KC_TRANSPARENT,KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,KC_F11,KC_CAPSLOCK,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_AUDIO_VOL_DOWN,KC_AUDIO_VOL_UP,KC_AUDIO_MUTE,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_MEDIA_PREV_TRACK,KC_MEDIA_NEXT_TRACK,KC_MEDIA_PLAY_PAUSE,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_F12,KC_F6,KC_F7,KC_F8,KC_F9,KC_F10,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_F14,KC_F15,KC_TRANSPARENT,KC_LEFT,KC_UP,KC_DOWN,KC_RIGHT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT),
+  [L2] = LAYOUT_ergodox(KC_TRANSPARENT,TO(0),TO(1),KC_TRANSPARENT,TO(3),KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_NUMLOCK,KC_KP_SLASH,KC_KP_ASTERISK,KC_KP_MINUS,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_KP_7,KC_KP_8,KC_KP_9,KC_KP_PLUS,KC_TRANSPARENT,KC_TRANSPARENT,KC_KP_4,KC_KP_5,KC_KP_6,KC_KP_PLUS,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_KP_1,KC_KP_2,KC_KP_3,KC_KP_ENTER,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_KP_DOT,KC_KP_ENTER,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_0),
+  [L3] = LAYOUT_ergodox(KC_EQUAL,KC_1,KC_2,KC_3,KC_4,KC_5,KC_ESCAPE,KC_TAB,KC_Q,KC_W,KC_E,KC_R,KC_T,KC_M,KC_LCTL,KC_A,KC_S,KC_D,KC_F,KC_G,KC_LSHIFT,KC_Z,KC_X,KC_C,KC_V,KC_B,KC_I,KC_CAPSLOCK,KC_GRAVE,KC_LCTL,KC_LALT,KC_LGUI,KC_LGUI,KC_LALT,KC_HOME,KC_SPACE,KC_E,CTL_T(KC_SPACE),TO(0),KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_F,KC_O,KC_R,KC_T,KC_TRANSPARENT,KC_TRANSPARENT,KC_N,KC_I,KC_T,KC_E,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT),
 };
 
 const uint16_t PROGMEM fn_actions[] = {
-    [1] = ACTION_LAYER_TAP_TOGGLE(L1)                // FN1 - Momentary Layer 1 (Function)
+  [1] = ACTION_LAYER_TAP_TOGGLE(L1)                // FN1 - Momentary Layer 1 (Function)
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
@@ -238,7 +73,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 // Runs just one time when the keyboard initializes.
 void matrix_init_user(void) {
-
+#ifdef RGBLIGHT_COLOR_LAYER_0
+  rgblight_setrgb(RGBLIGHT_COLOR_LAYER_0);
+#endif
 };
 
 
@@ -251,17 +88,61 @@ void matrix_scan_user(void) {
     ergodox_right_led_1_off();
     ergodox_right_led_2_off();
     ergodox_right_led_3_off();
-    switch (layer) {
-      // TODO: Make this relevant to the ErgoDox EZ.
-        case 1:
-            ergodox_right_led_1_on();
-            break;
-        case 2:
-            ergodox_right_led_2_on();
-            break;
-        default:
-            // none
-            break;
-    }
 
+    switch (layer) {
+      case 0:
+        #ifdef RGBLIGHT_COLOR_LAYER_0
+          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_0);
+        #endif
+        break;
+      case 1:
+        ergodox_right_led_1_on();
+        #ifdef RGBLIGHT_COLOR_LAYER_1
+          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_1);
+        #endif
+        break;
+      case 2:
+        ergodox_right_led_2_on();
+        #ifdef RGBLIGHT_COLOR_LAYER_2
+          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_2);
+        #endif
+        break;
+      case 3:
+        ergodox_right_led_3_on();
+        #ifdef RGBLIGHT_COLOR_LAYER_3
+          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_3);
+        #endif
+        break;
+      case 4:
+        ergodox_right_led_1_on();
+        ergodox_right_led_2_on();
+        #ifdef RGBLIGHT_COLOR_LAYER_4
+          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_4);
+        #endif
+        break;
+      case 5:
+        ergodox_right_led_1_on();
+        ergodox_right_led_3_on();
+        #ifdef RGBLIGHT_COLOR_LAYER_5
+          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_5);
+        #endif
+        break;
+      case 6:
+        ergodox_right_led_2_on();
+        ergodox_right_led_3_on();
+        #ifdef RGBLIGHT_COLOR_LAYER_6
+          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_6);
+        #endif
+        break;
+      case 7:
+        ergodox_right_led_1_on();
+        ergodox_right_led_2_on();
+        ergodox_right_led_3_on();
+        #ifdef RGBLIGHT_COLOR_LAYER_7
+          rgblight_setrgb(RGBLIGHT_COLOR_LAYER_6);
+        #endif
+        break;
+      default:
+        break;
+    }
 };
