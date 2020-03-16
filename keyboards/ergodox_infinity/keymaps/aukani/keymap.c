12286952071281866f93db664adfd82e127e39a8
Ali Ukani <aukani@hubspot.com>
2020-03-16 13:21:43 -0400

    update ergodox layout

--- keyboards/ergodox_infinity/keymaps/aukani/keymap.c
+++ keyboards/ergodox_infinity/keymaps/aukani/keymap.c
@@ -3,10 +3,9 @@
 #include "action_layer.h"
 #include "version.h"
 
-#define L0 0 // default layer
-#define L1 1 // media keys, movement
-#define L2 2 // numpad, layer select
-#define L3 3 // fortnite lol
+#define L0 0
+#define L1 1
+#define L2 2
 
 enum custom_keycodes {
   PLACEHOLDER = SAFE_RANGE, // can always be here
@@ -16,10 +15,93 @@ enum custom_keycodes {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [L0] = LAYOUT_ergodox(KC_EQUAL,KC_1,KC_2,KC_3,KC_4,KC_5,KC_ESCAPE,KC_TAB,KC_Q,KC_W,KC_E,KC_R,KC_T,KC_LBRACKET,KC_LCTL,KC_A,KC_S,KC_D,KC_F,KC_G,KC_LSPO,KC_Z,KC_X,KC_C,KC_V,KC_B,MO(1),KC_CAPSLOCK,KC_GRAVE,KC_LCTL,KC_LALT,KC_LGUI,KC_LGUI,KC_LALT,KC_HOME,KC_BSPACE,KC_DELETE,KC_END,TG(2),KC_6,KC_7,KC_8,KC_9,KC_0,KC_MINUS,KC_RBRACKET,KC_Y,KC_U,KC_I,KC_O,KC_P,KC_BSLASH,KC_H,KC_J,KC_K,KC_L,KC_SCOLON,KC_QUOTE,MO(2),KC_N,KC_M,KC_COMMA,KC_DOT,KC_SLASH,KC_RSPC,KC_RGUI,KC_RALT,MEH_T(KC_NO),ALL_T(KC_NO),TT(1),KC_RCTRL,KC_RGUI,KC_PGUP,KC_PGDOWN,KC_ENTER,KC_SPACE),
-  [L1] = LAYOUT_ergodox(KC_TRANSPARENT,KC_F1,KC_F2,KC_F3,KC_F4,KC_F5,KC_F11,KC_CAPSLOCK,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_AUDIO_VOL_DOWN,KC_AUDIO_VOL_UP,KC_AUDIO_MUTE,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_MEDIA_PREV_TRACK,KC_MEDIA_NEXT_TRACK,KC_MEDIA_PLAY_PAUSE,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,RESET,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_F12,KC_F6,KC_F7,KC_F8,KC_F9,KC_F10,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_F14,KC_F15,KC_TRANSPARENT,KC_LEFT,KC_UP,KC_DOWN,KC_RIGHT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,RESET,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT),
-  [L2] = LAYOUT_ergodox(KC_TRANSPARENT,TO(0),TO(1),KC_TRANSPARENT,TO(3),KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_NUMLOCK,KC_KP_SLASH,KC_KP_ASTERISK,KC_KP_MINUS,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_KP_7,KC_KP_8,KC_KP_9,KC_KP_PLUS,KC_TRANSPARENT,KC_TRANSPARENT,KC_KP_4,KC_KP_5,KC_KP_6,KC_KP_PLUS,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_KP_1,KC_KP_2,KC_KP_3,KC_KP_ENTER,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_KP_DOT,KC_KP_ENTER,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_0),
-  [L3] = LAYOUT_ergodox(KC_EQUAL,KC_1,KC_2,KC_3,KC_4,KC_5,KC_ESCAPE,KC_TAB,KC_Q,KC_W,KC_E,KC_R,KC_T,KC_M,KC_LCTL,KC_A,KC_S,KC_D,KC_F,KC_G,KC_LSHIFT,KC_Z,KC_X,KC_C,KC_V,KC_B,KC_I,KC_CAPSLOCK,KC_GRAVE,KC_LCTL,KC_LALT,KC_LGUI,KC_LGUI,KC_LALT,KC_HOME,KC_SPACE,KC_E,CTL_T(KC_SPACE),TO(0),KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_F,KC_O,KC_R,KC_T,KC_TRANSPARENT,KC_TRANSPARENT,KC_N,KC_I,KC_T,KC_E,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT),
+ /* Layer 0 (Base)
+  *
+  * ,--------------------------------------------------.           ,--------------------------------------------------.
+  * |   =    |   1  |   2  |   3  |   4  |   5  | LEFT |           | RIGHT|   6  |   7  |   8  |   9  |   0  |   -    |
+  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+  * | Del    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
+  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+  * | BkSp   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |' / Cmd |
+  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
+  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
+  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+  *   |Grv/L1|  '"  |AltShf| Left | Right|                                       |  Up  | Down |   [  |   ]  | ~L1  |
+  *   `----------------------------------'                                       `----------------------------------'
+  *                                        ,-------------.       ,-------------.
+  *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
+  *                                 ,------|------|------|       |------+--------+------.
+  *                                 |      |      | Home |       | PgUp |        |      |
+  *                                 | Space|Backsp|------|       |------|  Tab   |Enter |
+  *                                 |      |ace   | End  |       | PgDn |        |      |
+  *                                 `--------------------'       `----------------------'
+  */
+  // If it accepts an argument (i.e, is a function), it doesn't need KC_.
+  // Otherwise, it needs KC_*
+  [L0] = LAYOUT_ergodox(
+          // left hand
+          KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
+          KC_DELT,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
+          KC_BSPC,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
+          KC_LSPO,        KC_Z ,        KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
+          LT(SYMB,KC_GRV),KC_QUOT,      LALT(KC_LSFT),  KC_LEFT,KC_RGHT,
+                                                ALT_T(KC_APP),  KC_LGUI,
+                                                                KC_HOME,
+                                                KC_SPC,KC_BSPC,KC_END,
+          // right hand
+          KC_RGHT,     KC_6,   KC_7,  KC_8,   KC_9,   KC_0,      KC_MINS,
+          TG(SYMB),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,      KC_BSLS,
+                       KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,   KC_QUOT,
+          MEH_T(KC_NO),KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH,   KC_RSPC,
+                                KC_UP, KC_DOWN,KC_LBRC,KC_RBRC,  TT(SYMB),
+          KC_LALT,        CTL_T(KC_ESC),
+          KC_PGUP,
+          KC_PGDN,KC_TAB, KC_ENT
+  ),
+
+ /* Layer 1
+  *
+  * ,--------------------------------------------------.           ,--------------------------------------------------.
+  * |   =    |   1  |   2  |   3  |   4  |   5  | LEFT |           | RIGHT|   6  |   7  |   8  |   9  |   0  |   -    |
+  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+  * | Del    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
+  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+  * | BkSp   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|' / Cmd |
+  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
+  * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
+  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+  *   |Grv/L1|  '"  |AltShf| Left | Right|                                       |  Up  | Down |   [  |   ]  | ~L1  |
+  *   `----------------------------------'                                       `----------------------------------'
+  *                                        ,-------------.       ,-------------.
+  *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
+  *                                 ,------|------|------|       |------+--------+------.
+  *                                 |      |      | Home |       | PgUp |        |      |
+  *                                 | Space|Backsp|------|       |------|  Tab   |Enter |
+  *                                 |      |ace   | End  |       | PgDn |        |      |
+  *                                 `--------------------'       `----------------------'
+  */
+  // If it accepts an argument (i.e, is a function), it doesn't need KC_.
+  // Otherwise, it needs KC_*
+  [L1] = LAYOUT_ergodox(
+          // left hand
+          KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
+          KC_DELT,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
+          KC_BSPC,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
+          KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_C,   KC_V,   KC_B,   ALL_T(KC_NO),
+          LT(SYMB,KC_GRV),KC_QUOT,      LALT(KC_LSFT),  KC_LEFT,KC_RGHT,
+                                                ALT_T(KC_APP),  KC_LGUI,
+                                                                KC_HOME,
+                                                KC_SPC,KC_BSPC,KC_END,
+          // right hand
+          KC_RGHT,     KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_MINS,
+          TG(SYMB),    KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSLS,
+                        KC_H,   KC_J,  KC_K,   KC_L,   LT(MDIA, KC_SCLN),GUI_T(KC_QUOT),
+          MEH_T(KC_NO),KC_N,   KC_M,  KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
+                                KC_UP, KC_DOWN,KC_LBRC,KC_RBRC,          TT(SYMB),
+          KC_LALT,        CTL_T(KC_ESC),
+          KC_PGUP,
+          KC_PGDN,KC_TAB, KC_ENT
+  ),
 };
 
 const uint16_t PROGMEM fn_actions[] = {
